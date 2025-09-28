4b90ea7496303ab6189029fdfe7d6e64c384321d
S <harshatha.s@hp.com>
2025-09-28 14:11:11 +0530

    fixing more responsivness issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent {
   title = 'portfolio';
-  mobileQuery: MediaQueryList;
-
-  constructor(private breakpointObserver: BreakpointObserver) {
-    this.mobileQuery = matchMedia('(max-width: 768px)');
-  }
-
-  ngOnInit() {
-    // Listen for mobile query changes
-    this.mobileQuery.addEventListener('change', this.mobileQueryListener);
-  }
-
-  ngOnDestroy() {
-    this.mobileQuery.removeEventListener('change', this.mobileQueryListener);
-  }
-
-  private mobileQueryListener = () => {
-    // This will trigger change detection when mobile state changes
-  };
-
-  closeSidenavIfMobile(sidenav: any) {
-    if (this.mobileQuery.matches) {
-      sidenav.close();
-    }
-  }
 }
